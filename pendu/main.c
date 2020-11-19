23ba44c0ab7067a3400be182d4a46574c646ef21
marine schwartz <m.schwartz@ludus-academie.com>
2020-11-19 19:14:07 +0100

    correction du bug
    remplacement des * par & dans les arguments de la fonction
    
    --> plus d'erreur
    
    --> reste 4 avertissements, à voir

--- pendu/main.c
+++ pendu/main.c
@@ -57,7 +57,7 @@ int main()
         proposition = lecture();
         fflush(stdin);
 
-        *motDevine = comparaison(tailleMot, *compteur, *gagne, proposition, motDevine, motRand);
+        *motDevine = comparaison(tailleMot, &compteur, &gagne, proposition, motDevine, motRand);
     }
 
     return 0;
